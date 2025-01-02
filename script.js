8380c87eb5a94e38b35fb685c0fead4949b1f64c
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-01-02 16:52:42 +0100

    cambios en acordeon y empezar estrutura

--- script.js
+++
@@ -1,8 +0,0 @@
-const headers = document.querySelectorAll(".filosofia__acordeon__header");
-
-headers.forEach((header) => {
-  header.addEventListener("click", () => {
-    const content = header.nextElementSibling;
-    content.classList.toggle("open");
-  });
-});
